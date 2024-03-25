285ccef0d092f886e46223909a204a48560d3c4b
tjjoslin <124922955+tjjoslin@users.noreply.github.com>
2024-03-25 11:46:53 -0400

    Dice Code

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour
+{
+    Rigidbody rb;
+    bool hasLanded;
+    bool thrown;
+
+    Vector3 startPosition;
+
+    [SerializeField] int diceValue;
+
+    public DiceSide[] diceSides;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
+        rb.useGravity = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice();
+        }
+
+        if (rb.IsSleeping() && !hasLanded && thrown)
+        {
+            hasLanded = true;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            SideValueCheck();
+        }
+        else if (rb.IsSleeping() && hasLanded && diceValue == 0)
+        {
+            RollAgain();
+        }
+    }
+
+    void RollDice()
+    {
+        if (!thrown && !hasLanded)
+        {
+            thrown = true;
+            rb.useGravity = true;
+            rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+        }
+        else if (thrown && hasLanded)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        transform.position = startPosition;
+        thrown = false;
+        hasLanded = false;
+        rb.useGravity = false;
+        rb.isKinematic = false;
+    }
+    void RollAgain()
+    {
+        Reset();
+        thrown = true;
+        rb.useGravity = true;
+        rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+    }
+
+    void SideValueCheck()
+    {
+        diceValue = 0;
+        foreach (DiceSide side in diceSides)
+        {
+            if (side.OnGround())
+            {
+                diceValue = side.sideValue;
+                Debug.Log(diceValue + " has been rolled!");
+            }
+        }
+    }
+}
